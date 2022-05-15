ea859db73e03fb5f576e092bd90b0913a559e57c
Samuel Patmore <daydreamapplications@googlemail.com>
2022-05-15 17:45:30 +0100

    Integrate Hilt

--- app/src/main/java/com/daydreamapplications/gemgame/MainActivity.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/MainActivity.kt
@@ -3,7 +3,9 @@ package com.daydreamapplications.gemgame
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.daydreamapplications.gemgame.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

5ae23ab0559551a315c2f397ef651051bf799fa3
Samuel Patmore <daydreamapplications@googlemail.com>
2022-05-15 18:05:32 +0100

    Create Score interface

--- app/src/main/java/com/daydreamapplications/gemgame/game/MainActivity.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/game/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.daydreamapplications.gemgame.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Score {
+
+    override val current: MutableStateFlow<Int> = MutableStateFlow(0)
+
+    override fun change(by: Int) {
+        current.value += by
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

29161e2c1e05c86f7cbbbb0da4aef72a87821987
Samuel Patmore <daydreamapplications@googlemail.com>
2022-05-15 18:14:48 +0100

    Implement basic scores

--- app/src/main/java/com/daydreamapplications/gemgame/game/MainActivity.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/game/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.daydreamapplications.gemgame.game
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.daydreamapplications.gemgame.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), Score {
@@ -18,11 +21,23 @@ class MainActivity : AppCompatActivity(), Score {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         ActivityMainBinding.inflate(layoutInflater).apply {
 
             lifecycleOwner = this@MainActivity
+            gemGridView.score = this@MainActivity
+
+            observeCurrentScore(scoreValue)
 
             setContentView(root)
+        }
+    }
+
+    private fun observeCurrentScore(textView: TextView) {
+        lifecycleScope.launchWhenResumed {
+            current.collect {
+                textView.text = "Score: $it"
+            }
         }
     }
 }

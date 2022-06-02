3813b14321a35f5b02c1a35d2e0138eccd8778b6
Samuel Patmore <daydreamapplications@googlemail.com>
2022-06-02 14:39:20 +0100

    Implement score persistence using SharedPreferences

--- app/src/main/java/com/daydreamapplications/gemgame/idle/score/ScoreModule.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/idle/score/ScoreModule.kt
@@ -1,10 +1,15 @@
 package com.daydreamapplications.gemgame.idle.score
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.daydreamapplications.gemgame.game.Score
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -12,4 +17,15 @@ abstract class ScoreModule {
 
     @Binds
     abstract fun provideScore(scoreRepository: ScoreRepository): Score
+
+    companion object {
+
+        @Provides
+        @Named("Score")
+        fun provideScorePreferences(
+            @ApplicationContext context: Context,
+        ): SharedPreferences {
+            return context.getSharedPreferences("Score", Context.MODE_PRIVATE)
+        }
+    }
 }
--- app/src/main/java/com/daydreamapplications/gemgame/idle/score/ScoreRepository.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/idle/score/ScoreRepository.kt
@@ -1,17 +1,32 @@
 package com.daydreamapplications.gemgame.idle.score
 
+import android.content.SharedPreferences
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.core.content.edit
 import com.daydreamapplications.gemgame.game.Score
 import javax.inject.Inject
+import javax.inject.Named
 
 class ScoreRepository @Inject constructor(
+    // TODO: Wrapper class for Scores
+    @Named("Score")
+    private val sharedPreferences: SharedPreferences,
 ) : Score {
 
-    // TODO: persistence
-    override val current: MutableState<Long> = mutableStateOf(0)
+    override val current: MutableState<Long> = mutableStateOf(storedScore)
 
     override fun change(by: Number) {
-        current.value += by.toLong()
+        val newValue = storedScore + by.toLong()
+        storedScore = newValue
+        current.value = newValue
     }
+
+    private var storedScore: Long
+        get() = sharedPreferences.getLong("score", 0L)
+        set(value) {
+            sharedPreferences.edit {
+                putLong("score", value)
+            }
+        }
 }

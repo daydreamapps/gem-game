14316bd38101b989660f1a981a1bd755bfb6ef3f
Samuel Patmore <daydreamapplications@googlemail.com>
2022-06-06 22:42:54 +0100

    Refactor IdleGameTimings to use TimingPersistence

--- app/src/main/java/com/daydreamapplications/gemgame/idle/timing/IdleGameTimings.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/idle/timing/IdleGameTimings.kt
@@ -6,16 +6,22 @@ import com.daydreamapplications.gemgame.idle.upgrades.UpgradeType
 import javax.inject.Inject
 
 class IdleGameTimings @Inject constructor(
+    private val timingPersistence: TimingPersistence,
 ) : GameTimings {
 
-    override var dropDuration: Long = 100L
-    override var hideDuration: Long = 500L
-    override var swapDurationMs: Long = 500L
+    override val dropDuration: Long
+        get() = timingPersistence.dropDurationInMs
+
+    override val hideDuration: Long
+        get() = timingPersistence.hideDurationInMs
+
+    override val swapDurationMs: Long
+        get() = timingPersistence.swapDurationInMs
 
     fun applyUpgrade(upgrade: Upgrade) {
         when (upgrade.type) {
             UpgradeType.SWAP_SPEED -> {
-                swapDurationMs = (swapDurationMs * upgrade.multiplier).toLong()
+                timingPersistence.swapDurationInMs = (swapDurationMs * upgrade.multiplier).toLong()
             }
         }
     }

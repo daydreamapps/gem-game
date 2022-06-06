26c84634747292eac45f0cf4a5a1780b5e8deb4b
Samuel Patmore <daydreamapplications@googlemail.com>
2022-06-06 23:33:20 +0100

    UpdateRepository lists new types (non-functional)

--- app/src/main/java/com/daydreamapplications/gemgame/idle/upgrades/UpgradesRepository.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/idle/upgrades/UpgradesRepository.kt
@@ -17,25 +17,25 @@ class UpgradesRepository @Inject constructor() {
     companion object {
 
         private val stubUpgrades = listOf(
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 100, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 200, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 100, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 200, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 300, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 500, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 800, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 500, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 800, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 1300, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 2100, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 3400, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 2100, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 3400, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 5500, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 8900, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 14400, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 8900, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 14400, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 23300, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 37700, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 61000, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 37700, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 61000, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 98700, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 159700, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 258400, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 159700, multiplier = 0.9),
+            Upgrade(type = UpgradeType.REMOVAL_SPEED, cost = 258400, multiplier = 0.9),
             Upgrade(type = UpgradeType.SWAP_SPEED, cost = 418100, multiplier = 0.9),
-            Upgrade(type = UpgradeType.SWAP_SPEED, cost = 676500, multiplier = 0.9),
+            Upgrade(type = UpgradeType.DROP_SPEED, cost = 676500, multiplier = 0.9),
         )
     }
 }

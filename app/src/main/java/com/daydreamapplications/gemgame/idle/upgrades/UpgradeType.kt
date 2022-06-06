05328958b6e5e73bc15f0d2ac439b569dc796f41
Samuel Patmore <daydreamapplications@googlemail.com>
2022-06-06 23:30:06 +0100

    New upgrade types: Removal & drop speed

--- app/src/main/java/com/daydreamapplications/gemgame/idle/upgrades/UpgradeType.kt
+++ app/src/main/java/com/daydreamapplications/gemgame/idle/upgrades/UpgradeType.kt
@@ -3,5 +3,7 @@ package com.daydreamapplications.gemgame.idle.upgrades
 enum class UpgradeType(
     val title: String,
 ) {
+    DROP_SPEED(title = "Drop Speed"),
+    REMOVAL_SPEED(title = "Removal  Speed"),
     SWAP_SPEED(title = "Swap Speed"),
 }
